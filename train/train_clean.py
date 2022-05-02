6a9e7ef2638d252bb1706319b6a8b989f72845d8
Aniruddha Chauhan <ac8826@log-3.hpc.nyu.edu>
2022-05-02 11:46:56 -0400

    Bug fix

--- train/train_clean.py
+++ train/train_clean.py
@@ -42,7 +42,7 @@ def train(args):
 		if args.model == 'bert':
 			tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 		elif args.model == 'distilbert':
-			tokenizer = DistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
+			tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 		elif args.model == 'roberta':
 			tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
 		elif args.model == 'deberta':
@@ -108,7 +108,7 @@ def train(args):
 			output_dir=out_dir,          # output directory to where save model checkpoint
 			# evaluation_strategy="steps",    # evaluate each `logging_steps` steps
 			overwrite_output_dir=True,      
-			num_train_epochs=50,            # number of training epochs, feel free to tweak
+			num_train_epochs=25,            # number of training epochs, feel free to tweak
 			per_device_train_batch_size=16, # the training batch size, put it as high as your GPU memory fits
 			gradient_accumulation_steps=1,  # accumulating the gradients before updating the weights
 			per_device_eval_batch_size=64,  # evaluation batch size
